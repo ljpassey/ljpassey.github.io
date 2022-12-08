d8eafc651a5783541f615061180dfe81d0a92358
Luke Passey <ljpassey@gmail.com>
2022-12-07 17:31:33 -0700

    This commit includes some updates to my portfolio page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "*.{html,js}",
-  ],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [require("daisyui")],
-}
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+};
